c13a7bc9b2ccceb835c3a564bc69ac49d24ba309
CamilleAbella <camille.abella@hotmail.fr>
2021-09-21 11:50:08 +0200

    add fight system

--- src/namespaces/fight.ts
+++ src/namespaces/fight.ts
@@ -1,18 +1,50 @@
 export function startFight(fighters: [Fighter, Fighter]) {
-  let elapsedTime = 0
+  const ctx: FightContext = {
+    elapsedTime: -1,
+  }
+
+  // game loop
+  while (fighters.every((fighter) => fighter.stats.hp.value > 0)) {
+    ctx.elapsedTime++
+
+    const actions: Action[] = []
+
+    for (const fighter of fighters) {
+      if (fighter.canPlay(ctx)) {
+        actions.push(fighter.getAction(ctx))
+      }
+    }
+
+    for (const action of actions.sort((a, b) => a.priority - b.priority)) {
+      action.run(ctx)
+    }
+  }
 }
 
 export class Fighter {
+  public lastElapsedTime = 0
+
   constructor(
     public name: string,
     public stats: {
-      hp: number
-      speed: number
-      luck: number
-      strength: number
-      energy: number
+      hp: Stat
+      speed: Stat
+      luck: Stat
+      strength: Stat
+      energy: Stat
     }
   ) {}
+
+  canPlay(ctx: FightContext): boolean {
+    return (
+      this.lastElapsedTime <
+      ctx.elapsedTime + this.stats.speed.finalValue / this.stats.speed.value
+    )
+  }
+
+  getAction(ctx: FightContext): Action {
+    return new Action(1)
+  }
 }
 
 export class Stat {
@@ -24,4 +56,27 @@ export class Stat {
   ) {
     this.value = initialValue
   }
+
+  get factor(): number {
+    return (
+      this.value /
+      (this.initialValue < this.finalValue
+        ? this.finalValue
+        : this.initialValue)
+    )
+  }
+
+  get percents(): number {
+    return this.factor * 100
+  }
+}
+
+export class Action {
+  constructor(public readonly priority: number) {}
+
+  run(ctx: FightContext) {}
+}
+
+export interface FightContext {
+  elapsedTime: number
 }
